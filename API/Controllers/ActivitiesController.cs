9ea23336f834d60236ec2985812b8b89b97ce74b
Anil Kumar <786.anil.potlapally@gmail.com>
2021-12-24 19:09:21 +0530

    just 3 rd commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+
+namespace API.Controllers
+{
+  
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ActivitiesController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivties(){
+            
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")] //activity/id
+        public async Task<ActionResult<Activity>> GetActivties(Guid id){
+            
+            return await _context.Activities.FindAsync(id);
+        }
+
+    }
+}
